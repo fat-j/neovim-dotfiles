b53d01184cd6ac211a441e08cf9e9aa4c3809bb9
fat-j <103350887+fat-j@users.noreply.github.com>
2023-11-10 23:42:45 -0700

    No idea what changed

--- nvim/lua/core/colors.lua
+++ nvim/lua/core/colors.lua
@@ -3,7 +3,7 @@
 --vim.cmd.colorscheme 'kanagawa-wave' -- kanagawa-wave, kangawa-dragon, kangawa-lotus
 
 --vim.cmd.colorscheme 'tokyonight-night' -- tokyonight-night, tokyonight-storm, tokyonight-day, tokyonight-moon
- 
+
 --vim.cmd.colorscheme 'slate'
 
 --vim.cmd.colorscheme 'gruvbox'
@@ -16,3 +16,4 @@ vim.cmd(":hi DiagnosticVirtualTextWarn guifg=#e0af68")
 vim.cmd(":hi DiagnosticVirtualTextHint guifg=#10B981")
 vim.cmd(":hi DiagnosticVirtualTextInfo guifg=#0db9d7")
 vim.cmd(":hi DiagnosticVirtualTextError guifg=#db4b4b")
+vim.cmd(":hi Visual guibg=#FFFFFF")
